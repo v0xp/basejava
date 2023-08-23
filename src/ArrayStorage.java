26298023b22d76b65da9b3e4fab5c4eb98fafb0f
Вячеслав Киселёв <vskiseleff@yandex.ru>
2023-08-23 20:23:18 +0300

    Создаем счетчик резюме

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
     }
--- src/Resume.java
+++ src/Resume.java
@@ -8,6 +8,7 @@ public class Resume {
 
     @Override
     public String toString() {
+
         return uuid;
     }
 }
