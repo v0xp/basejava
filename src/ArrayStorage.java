d8c7f83303ffc1618d912fb002f261e09c97cc2e
Вячеслав Киселёв <vskiseleff@yandex.ru>
2023-08-24 18:13:25 +0300

    очищаем массив резюме (заполняем зачением null занятые резюме ячейки) и обнуляем сетчик резюме

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,13 +7,17 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size;
 
+    // очищаем массив резюме (заполняем зачением null занятые резюме ячейки) и обнуляем сетчик резюме
     void clear() {
+        Arrays.fill(storage,0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
     }
 
     Resume get(String uuid) {
+
         return null;
     }
 
@@ -24,10 +28,12 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+
+        return size;
     }
 }
